ea3874fd72b8d285af9153f6cdfd00bafe93240f
pengwk <pengwk2@gmail.com>
2016-07-17 16:00:26 +0800

    more easy to read

--- chapter_2/p16_code.py
+++ chapter_2/p16_code.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env 
+#!/usr/bin/env
 #_*_ coding: utf-8 _*_
 
-# 
+#
+
+
 def largest_odd(x, y, z):
     """
     >>> largest_odd(1, 3, 4)
@@ -10,20 +12,31 @@ def largest_odd(x, y, z):
     No odd!
     >>> largest_odd(3, 8, 16)
     3
+    >>> largest_odd(17, 8, 16)
+    17
+    >>> largest_odd(13, 13, 12)
+    13
+    >>> largest_odd(13, 12, 12)
+    13
     """
-    if x%2 != 0 or y%2 != 0 or z%2 != 0:
-        if x < y:
-            x, y = y, x
-        if x < z:
-            x, z = z, x
-        if y < z:
-            y, z = z, y
-        if x%2 == 1:
-            print x
-        elif y%2 == 1:
-            print y
-        elif z%2 == 1:
-            print z
+    _big, _mid, _small = x, y, z
+
+    if _big % 2 != 0 or _mid % 2 != 0 or _small % 2 != 0:
+        if _big < _mid:
+            _big, _mid = _mid, _big
+
+        if _big < _small:
+            _big, _small = _small, _big
+
+        if _mid < _small:
+            _mid, _small = _small, _mid
+
+        if _big % 2 == 1:
+            print _big
+        elif _mid % 2 == 1:
+            print _mid
+        elif _small % 2 == 1:
+            print _small
     else:
         print "No odd!"
 
